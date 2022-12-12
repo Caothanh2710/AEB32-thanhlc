2d6baac1f5d6c40b9176e91f401f25bce815191c
lecaothanh <lecaothanh2710@gmail.com>
2022-12-12 06:02:53 -0800

    thuchanhtrenlop

---
+++ hoc/session4/basic.js
@@ -0,0 +1,30 @@
+let num = 1;
+console.log("Number", num);
+let str = "thanh";
+console.log("string", str);
+let boo = true;
+console.log("boolean", boo);
+let und 
+let unde = undefined;
+console.log("undefined, und , unde");
+let nu = null;
+console.log("null", nu);
+
+let user = {
+  name: "ten user",
+  age: "tuoi user",
+};
+console.log("object , user");
+console.log("object.name", user.nae);
+
+let ar = [1, 2, 3, 4];
+console.log("Array<number>", ar);
+console.log("lay phan tu,ar[0]");
+
+let age = 25;
+
+console.log("Number", num);
+console.log("Age", age);
+
+const name = "thanh"
+console.log();
